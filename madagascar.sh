606663f47a269dc8f950abdcc4d263df95cfe362
hzyangzhou <hzyangzhou@corp.netease.com>
2013-09-26 10:30:09 +0800

    qa18 config

--- madagascar.sh
+++ madagascar.sh
@@ -1,22 +1,36 @@
 #!/usr/bin/bash
 
-baseDir="/home/study/rsync-production-logs"
-src1=${baseDir}"/1/access-study.log.1"
-src2=${baseDir}"/2/access-study.log.1"
-dest1=${baseDir}"/1/access-study.json"
-dest2=${baseDir}"/2/access-study.json"
-cpDest="/home/study/webroot-study/log-processed"
+keytab="/home/qatest/yz/study.keytab"
+baseDir="/home/qatest/yz/madagascar"
+src1=${baseDir}"/nginx.log"
+dest1=${baseDir}"/nginx.json"
+cpDest="/home/qatest/webroot-study/access-study.json"
+jsonBase="/home/qatest/webroot-study/access-study"
+
+## 0 get log
+
+kinit -kt ${keytab} study/dev@HADOOP.HZ.NETEASE.COM
+kinit -R
+$HADOOP_HOME/bin/hadoop fs -text /datastream/study/nginx/$(date -d yesterday +%Y_%m_%d)/* > nginx.log
 
 ## 1 process
-python ${baseDir}/scripts/logProcessor.py ${src1} ${dest1} 
+python ${baseDir}/logProcessor.py ${src1} ${dest1}
 
-python ${baseDir}/scripts/logProcessor.py ${src2} ${dest2}
 
 ## 2 merge
-python ${baseDir}/scripts/resultMerger.py ${dest1} ${dest2} access-study.json
+## python ${baseDir}/resultMerger.py ${dest1} ${dest2} access-study.json
 
 ## 3 post process
 
-python ${baseDir}/scripts/renderer.py access-study.json
+python ${baseDir}/renderer.py ${dest1}
 
-cp access-study.json ${cpDest}
+## 4 log rotate
+if [ -f ${cpDest} ]
+then
+  cp ${cpDest} ${jsonBase}/access-study-$(date -d ' 2 days ago' +%Y_%m_%d).json
+fi
+
+cp ${dest1} ${cpDest}
+
+## 5 clean
+rm ${src1}
